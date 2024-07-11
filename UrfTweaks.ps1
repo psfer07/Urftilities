08e3986cc96627f2923d34def1f4055be3bf74b4
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-07-11 17:50:18 +0100

    Minor fixes and UI improvements

--- UrfTweaks.ps1
+++ UrfTweaks.ps1
@@ -5,6 +5,14 @@ param (
     [Alias("r")][switch]$restart
 )
 
+# Bypass any execution policy
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+# Some customization
+$Host.UI.RawUI.BackGroundColor = 'Black'
+$Host.UI.RawUI.WindowTitle = 'Urftilities'
+Clear-Host
+
 if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit 1
@@ -25,7 +33,7 @@ if ($silent) {
 Import-Module -DisableNameChecking "$PSScriptRoot\modules\titles.psm1"
 
 Clear-Host
-if ($full) { Write-Warning `n"Full mode selected. This will last a lot more compared to the normal mode. Reboot is highly recommended" }
+if ($full -and !$restart) { Write-Warning `n"Full mode selected. This will last a lot more compared to the normal mode. Reboot is highly recommended" }
 if ($normal) {
     Write-Step "Removing temporary files"
     Write-Host `n"--> Deleting temporary files with cleanmgr..."
@@ -41,6 +49,14 @@ if ($normal) {
     Write-Host `n"--> Removing network cache..."
     arp -d * | Out-Null; nbtstat -RR | Out-Null; ipconfig /flushdns | Out-Null; ipconfig /registerdns | Out-Null
     Write-Host "Done!"
+}
+elseif (!($normal -or $full)) {
+    Write-Step "Select Urftilities mode"
+    Write-Host `n`n"You can choose between normal and full mode:"`n
+    Write-Host "1. Normal mode"
+    Write-Host "2. Full mode"
+    $mode = Read-Host "-->"
+
 }
 if ($full) {
     Write-Step "Creating a restore point"

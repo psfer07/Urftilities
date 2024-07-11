52ed197bd9957d0d2caf9e99492ac6680efa2942
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-07-11 20:57:04 +0100

    Fixed function logic
    It was all wrong from the begining.

--- modules/edit-regs.psm1
+++ modules/edit-regs.psm1
@@ -9,7 +9,7 @@ function Set-RegistryItem {
     )
 
     if ($Path.Length -gt 1) {
-        if ($Value.Length -eq 1 -and $Type.Length -eq 1) {
+        if ($Value.Length -eq $Type.Length -eq 1) {
             foreach ($keyPath in $Path) {
                 # Check if the path exists, creates it if not
                 if (!(Test-Path $keyPath)) { New-Item -Path $keyPath -Force | Out-Null }
@@ -26,45 +26,23 @@ function Set-RegistryItem {
         }
     }
     elseif ($Path.Length -eq 1) {
-        if ($Name.Length -eq $Value.Length -and $Value.Length -eq $Type.Length) {
-            for ($i = 0; $i -lt $Value.Length; $i++) {
-                Set-ItemProperty -Path $Path -Name $Name[$i] -Value $Value[$i] -Type $Type[$i] -ErrorAction SilentlyContinue
+        if ($Name.Length -eq $Value.Length -and $Type.Length -eq 1) {
+            for ($i = 0; $i -lt $Name.Length; $i++) {
+                if (!(Test-Path "$Path\$($Name[$i])")) { New-Item -Path "$Path\$($Name[$i])" -Force | Out-Null }
+                Set-ItemProperty -Path $Path -Name $($Name[$i]) -Value $($Value[$i]) -Type $Type -ErrorAction SilentlyContinue
             }
-            Pause
-            Write-Host "--> Path provided: " $Path
-            Write-Host "--> Names provided: " $Name
-            Write-Host "--> Values provided: " $Value
-            Write-Host "--> Types provided: " $Type
         }
-        elseif ($Name.Length -eq $Value.Length -and $Type.Length -eq 1) {
-            for ($i = 0; $i -lt $Value.Length; $i++) {
-                Set-ItemProperty -Path $Path -Name $Name[$i] -Value $Value[$i] -Type $Type -ErrorAction SilentlyContinue
+        elseif ($Value.Length -eq $Type.Length -eq 1) {
+            for ($i = 0; $i -lt $Name.Length; $i++) {
+                if (!(Test-Path "$Path\$($Name[$i])")) { New-Item -Path "$Path\$($Name[$i])" -Force | Out-Null }
+                Set-ItemProperty -Path $Path -Name $($Name[$i]) -Value $Value -Type $Type -ErrorAction SilentlyContinue
             }
-            Pause
-            Write-Host "--> Path provided: " $Path
-            Write-Host "--> Names provided: " $Name
-            Write-Host "--> Values provided: " $Value
-            Write-Host "--> Types provided: " $Type
         }
-        elseif ($Name.Length -eq $Type.Length -and $Type.Length -eq 1) {
-            foreach ($i in $Value) {
-                Set-ItemProperty -Path $Path -Name $Name -Value $i -Type $Type -ErrorAction SilentlyContinue
+        elseif ($Name.Length -eq $Value.Length -eq $Type.Length) {
+            for ($i = 0; $i -lt $Name.Length; $i++) {
+                if (!(Test-Path "$Path\$($Name[$i])")) { New-Item -Path "$Path\$($Name[$i])" -Force | Out-Null }
+                Set-ItemProperty -Path $Path -Name $($Name[$i]) -Value $($Value[$i]) -Type $($Type[$i]) -ErrorAction SilentlyContinue
             }
-            Pause
-            Write-Host "--> Path provided: " $Path
-            Write-Host "--> Names provided: " $Name
-            Write-Host "--> Values provided: " $Value
-            Write-Host "--> Types provided: " $Type
-        }
-        elseif ($Value.Length -eq $Type.Length -and $Type.Length -eq 1) {
-            foreach ($i in $Name) {
-                Set-ItemProperty -Path $Path -Name $i -Value $Value -Type $Type -ErrorAction SilentlyContinue
-            }
-            Pause
-            Write-Host "--> Path provided: " $Path
-            Write-Host "--> Names provided: " $Name.GetType().FullName
-            Write-Host "--> Values provided: " $Value.GetType().FullName
-            Write-Host "--> Types provided: " $Type
         }
         else {
             Write-Warning "Error: Mismatch between lengths of Name, Value, and Type arrays."
@@ -72,12 +50,10 @@ function Set-RegistryItem {
             Write-Host "--> Number of names provided: " $Name.Length
             Write-Host "--> Number of values provided: " $Value.Length
             Write-Host "--> Number of types provided: " $Type.Length
-            Pause
-            Write-Host "--> Path provided: " $Path
+            Write-Host `n`n"--> Path provided: " $Path
             Write-Host "--> Names provided: " $Name
             Write-Host "--> Values provided: " $Value
             Write-Host "--> Types provided: " $Type
-            Pause
         }
     }
 }

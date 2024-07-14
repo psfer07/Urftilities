bc5f544c4578a7291b40ad8f0bbba3bebe305179
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-07-14 19:12:27 +0200

    Another one

--- Deploy.bat
+++ Deploy.bat
@@ -7,21 +7,25 @@ set "zipFile=%temp%\Urftilities.zip"
 set "extractTo=%userprofile%\Desktop\Urftilities-1.1"
 set "args="
 
+REM Check if Urftilities is already located on the desktop
 if exist "%extractTo%" (
-    set /p "open=Urftilities is already located in your desktop. Do you want open it now? (y/n) -->"
+    set /p "open=Urftilities is already located on your desktop. Do you want to open it now? (y/n) --> "
 )
 
-REM Create the Urftilities folder on Desktop
+REM Create the Urftilities folder on Desktop if it doesn't exist and download/extract the ZIP file
 if not exist "%extractTo%" (
     mkdir "%extractTo%"
-    REM Download and extracts the ZIP file using PowerShell
+    REM Download and extract the ZIP file using PowerShell
     powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%zipFile%'"
     powershell -Command "Expand-Archive -Path '%zipFile%' -DestinationPath '%userprofile%\Desktop' -Force"
     del "%zipFile%"
-    set /p "open=Urftilities has been downloaded and extracted to your desktop. Do you want to execute it? (y/n) -->"
+    set /p "open=Urftilities has been downloaded and extracted to your desktop. Do you want to execute it? (y/n) --> "
 )
 
+REM Check the user's response and exit if not 'y'
 if /i "%open%" neq "y" exit /b 0
+
+REM Clear the screen and present the options to the user
 cls
 echo Choose a number to execute Urftilities...
 echo 1. Open Urftilities in regular mode (it just cleans temporary files).
@@ -34,10 +38,11 @@ echo 4. Open Urftilities silently in full mode (no prompts).
 echo ---------------------------------------------------------------------------
 echo ------------------------------  Restart mode  -----------------------------
 echo ---------------------------------------------------------------------------
-echo 5. Open Urftilities in full mode.
-echo 6. Open Urftilities in full mode silently.
-choice /c 123456 /n /m ""
+echo 5. Open Urftilities in full mode with restart.
+echo 6. Open Urftilities in full mode silently with restart.
+choice /c 123456 /n /m "Choose an option: "
 
+REM Capture the choice and set arguments accordingly
 set mode=%errorlevel%
 if %mode%==6 (
     set args=-full -silent -restart
@@ -52,4 +57,6 @@ if %mode%==6 (
 ) else if %mode%==1 (
     set args=-normal
 )
-powershell -Command "Start-Process powershell -ArgumentList '. %extractTo%\Urftweaks.ps1 %args%' -Verb RunAs"
+
+REM Execute the PowerShell script with the appropriate arguments with elevated privileges
+powershell -Command "Start-Process powershell -ArgumentList '.\%extractTo%\Urftweaks.ps1 %args%' -Verb RunAs"

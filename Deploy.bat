cd511f9cd921f83508516daf88647193ad0014cd
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-07-14 19:18:13 +0200

    Removed opening functionality after download
    It didn't work before so I guess nothing changes

--- Deploy.bat
+++ Deploy.bat
@@ -10,6 +10,43 @@ set "args="
 REM Check if Urftilities is already located on the desktop
 if exist "%extractTo%" (
     set /p "open=Urftilities is already located on your desktop. Do you want to open it now? (y/n) --> "
+    if /i "%open%" neq "y" exit /b 0
+
+    REM Clear the screen and present the options to the user
+    cls
+    echo Choose a number to execute Urftilities...
+    echo 1. Open Urftilities in regular mode (it just cleans temporary files).
+    echo 2. Open Urftilities in full mode (execute all the available optimizations).
+    echo ---------------------------------------------------------------------------
+    echo ------------------------------  Silent mode  ------------------------------
+    echo ---------------------------------------------------------------------------
+    echo 3. Open Urftilities silently in regular mode (no prompts).
+    echo 4. Open Urftilities silently in full mode (no prompts).
+    echo ---------------------------------------------------------------------------
+    echo ------------------------------  Restart mode  -----------------------------
+    echo ---------------------------------------------------------------------------
+    echo 5. Open Urftilities in full mode with restart.
+    echo 6. Open Urftilities in full mode silently with restart.
+    choice /c 123456 /n /m "Choose an option: "
+
+    REM Capture the choice and set arguments accordingly
+    set mode=%errorlevel%
+    if %mode%==6 (
+        set args=-full -silent -restart
+    ) else if %mode%==5 (
+        set args=-full -restart
+    ) else if %mode%==4 (
+        set args=-full -silent
+    ) else if %mode%==3 (
+        set args=-normal -silent
+    ) else if %mode%==2 (
+        set args=-full
+    ) else if %mode%==1 (
+        set args=-normal
+    )
+
+    REM Execute the PowerShell script with the appropriate arguments with elevated privileges
+    powershell -Command "Start-Process powershell -ArgumentList '.\%extractTo%\Urftweaks.ps1 %args%' -Verb RunAs"
 )
 
 REM Create the Urftilities folder on Desktop if it doesn't exist and download/extract the ZIP file
@@ -19,44 +56,4 @@ if not exist "%extractTo%" (
     powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%zipFile%'"
     powershell -Command "Expand-Archive -Path '%zipFile%' -DestinationPath '%userprofile%\Desktop' -Force"
     del "%zipFile%"
-    set /p "open=Urftilities has been downloaded and extracted to your desktop. Do you want to execute it? (y/n) --> "
 )
-
-REM Check the user's response and exit if not 'y'
-if /i "%open%" neq "y" exit /b 0
-
-REM Clear the screen and present the options to the user
-cls
-echo Choose a number to execute Urftilities...
-echo 1. Open Urftilities in regular mode (it just cleans temporary files).
-echo 2. Open Urftilities in full mode (execute all the available optimizations).
-echo ---------------------------------------------------------------------------
-echo ------------------------------  Silent mode  ------------------------------
-echo ---------------------------------------------------------------------------
-echo 3. Open Urftilities silently in regular mode (no prompts).
-echo 4. Open Urftilities silently in full mode (no prompts).
-echo ---------------------------------------------------------------------------
-echo ------------------------------  Restart mode  -----------------------------
-echo ---------------------------------------------------------------------------
-echo 5. Open Urftilities in full mode with restart.
-echo 6. Open Urftilities in full mode silently with restart.
-choice /c 123456 /n /m "Choose an option: "
-
-REM Capture the choice and set arguments accordingly
-set mode=%errorlevel%
-if %mode%==6 (
-    set args=-full -silent -restart
-) else if %mode%==5 (
-    set args=-full -restart
-) else if %mode%==4 (
-    set args=-full -silent
-) else if %mode%==3 (
-    set args=-normal -silent
-) else if %mode%==2 (
-    set args=-full
-) else if %mode%==1 (
-    set args=-normal
-)
-
-REM Execute the PowerShell script with the appropriate arguments with elevated privileges
-powershell -Command "Start-Process powershell -ArgumentList '.\%extractTo%\Urftweaks.ps1 %args%' -Verb RunAs"

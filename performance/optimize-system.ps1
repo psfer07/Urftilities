e6a0000faaafc327bcbac96058f6fa127d2b2694
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-07-27 16:15:36 +0100

    Update optimize-system.ps1
    Removing more bugging features

--- performance/optimize-system.ps1
+++ performance/optimize-system.ps1
@@ -11,12 +11,6 @@ Import-Module -DisableNameChecking "$PSScriptRoot\..\modules\titles.psm1"
 Write-Step "Improving performance"
 Write-Host "--> Optimizing system..."
 
-Write-Host `n"Optimizing general settings..."
-Set-RegistryItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Value 0
-Set-RegistryItem -Path "HKLM:\SYSTEM\ControlSet001\Services\Ndu" -Name "Start" -Value 4
-Remove-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" -Recurse -ErrorAction SilentlyContinue
-Write-Host "Done!"
-
 Write-Host `n"Optimizing svchost.exe usage of memory..."
 $RamInKB = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1KB
 Set-RegistryItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Value $RamInKB
@@ -31,10 +25,6 @@ Write-Host `n"Disabling hibernation..."
 Set-RegistryItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -Value 0
 Set-RegistryItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" -Name "PowerThrottlingOff" -Value 1
 Set-RegistryItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power" -Name "HibernateEnabled" -Value 0
-Write-Host "Done!"
-
-Write-Host `n"Decreasing processes kill time and menu show delay"
-Set-RegistryItem -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay", "AutoEndTasks", "LowLevelHooksTimeout", "WaitToKillServiceTimeout", "HungAppTimeout" -Value 0, 1, 1000, 2000, 4000
 Write-Host "Done!"
 
 Write-Host `n"Disabling Windows auto maintenance tasks..."
